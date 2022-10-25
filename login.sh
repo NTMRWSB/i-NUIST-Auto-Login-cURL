b4e6fd113b08db9f2c1f0284533c591255e22f6d
Jun Wang <wzx20020111@outlook.com>
2022-10-26 00:42:38 +0800

    Create login.sh

---
+++ login.sh
@@ -0,0 +1,12 @@
+curl 'http://a.nuist.edu.cn/api/v1/login' \
+  -H 'Accept: application/json, text/plain, */*' \
+  -H 'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6' \
+  -H 'Connection: keep-alive' \
+  -H 'Content-Type: application/json;charset=UTF-8' \
+  -H 'DNT: 1' \
+  -H 'Origin: http://a.nuist.edu.cn' \
+  -H 'Referer: http://a.nuist.edu.cn/authentication' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.27' \
+  --data-raw '{"username":"114514","password":"1919","channel":"4","ifautologin":"0","pagesign":"secondauth","usripadd":"10.12.240.123"}' \
+  --compressed \
+  --insecure
